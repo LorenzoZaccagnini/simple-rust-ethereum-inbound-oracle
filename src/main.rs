17f006476384bb9f24d1b6a4cb98d2a87432ef82
Lorenzo Zaccagnini <lorenzozcg@gmail.com>
2022-10-22 22:51:44 +0200

    ens

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use dotenv::dotenv;
+use ethnum::U256;
 use web3;
+use web3::futures::{future, StreamExt};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -7,7 +9,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let alchemy_api_key = dotenv::var("ALCHEMY_API_KEY").expect("ALCHEMY_API_KEY must be set");
     let web3 = web3::Web3::new(web3::transports::WebSocket::new(&alchemy_api_key).await?);
 
-    let contract_address = "0x06012c8cf97BEaD5deAe237070F9587f8E7A266d";
+    let contract_address = "0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85";
     let event_signature = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
 
     let filter = web3::types::FilterBuilder::default()
@@ -24,8 +26,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let transfer_listen = web3.eth_subscribe().subscribe_logs(filter).await?;
 
     transfer_listen
-        .for_each(|log| async {
-            println!("log: {:?}", log);
+        .for_each(|log| {
+            //println!("log: {:?}", log);
+            //decode event token id data
+            let id = format!("{:?}", log.unwrap().topics[3]);
+            println!("id NOT decoded: {:?}", id);
+            let id_decoded = U256::from_str_radix(&id[2..], 16).unwrap();
+            println!("id decoded: {:?}", id_decoded);
+            println!("----------");
+            future::ready(())
         })
         .await;
 
